e8e9bf7d57de4ac1deabc480bd47488bb0ceb61e
100451296 <98945796+100451296@users.noreply.github.com>
2023-04-11 16:16:41 +0200

    constantes en funciones.py

--- funciones.py
+++ funciones.py
@@ -4,6 +4,30 @@ import csv
 CSV_ENCENDIDO = "./ENCENDIDO.csv"
 CSV_APAGADO =  "./APAGADO.csv"
 
+COSTE_APAGAR = 1
+COSTE_ENCENDIDO = 2
+
+INDICES = {
+    "16" : 0,
+    "16.5" : 1,
+    "17" : 2,
+    "17.5" : 3,
+    "18" : 4,
+    "18.5" : 5,
+    "19" : 6,
+    "19.5" : 7,
+    "20" : 8,
+    "20.5" : 9,
+    "21" : 10,
+    "21.5" : 11,
+    "22" : 12,
+    "22.5" : 13,
+    "23" : 14,
+    "23.5" : 15,
+    "24" : 16,
+    "24.5" : 17,
+    "25" : 18,
+}
 
 def leer_csv(path: str) -> list:
      # Abre el archivo csv
@@ -29,3 +53,10 @@ def leer_csv(path: str) -> list:
 
 
     return matriz
+
+
+
+if __name__  == "__main__":
+
+    print("hola")   
+    pass
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     transiciones_apagado = leer_csv(CSV_APAGADO)
     transiciones_encendido = leer_csv(CSV_ENCENDIDO)
 
-    print(transiciones_encendido)
+    print(len(transiciones_apagado))
 
         
         
